4abf79e48df26f1bb23298f89c575d15183c56cc
AlphaWolf2020 <65351771+AlphaWolf2020@users.noreply.github.com>
2020-05-18 22:59:49 +0100

    Model loader fix
    i hope...

--- src/main/java/com/ews/blocks/BlockBase.java
+++ src/main/java/com/ews/blocks/BlockBase.java
@@ -10,6 +10,8 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
+import java.util.Objects;
+
 public class BlockBase extends Block implements IHasModel {
 
     public BlockBase(String name, Material material){
@@ -18,7 +20,7 @@ public class BlockBase extends Block implements IHasModel {
         setRegistryName(name);
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
         ModBlocks.BLOCKS.add(this);
-        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(Objects.requireNonNull(this.getRegistryName())));
     }
 
 
