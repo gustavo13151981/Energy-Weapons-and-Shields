6c49a8243ae65e3ccf1aed1596cee619640a2d75
AlphaWolf2020 <65351771+AlphaWolf2020@users.noreply.github.com>
2020-05-23 15:01:15 +0100

    Ruby Armour made and Textured !
    ADDED: Ruby Axe recipe and Ruby Pickaxe recipe

--- src/main/java/com/ews/init/ModItems.java
+++ src/main/java/com/ews/init/ModItems.java
@@ -1,12 +1,16 @@
 package com.ews.init;
 
 import com.ews.items.ItemBase;
+import com.ews.items.armor.ArmorBase;
 import com.ews.items.tools.ToolAxe;
 import com.ews.items.tools.ToolHoe;
 import com.ews.items.tools.ToolPickaxe;
 import com.ews.items.tools.ToolSpade;
 import com.ews.items.tools.ToolSword;
+import com.ews.util.Reference;
 
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemHoe;
@@ -14,6 +18,7 @@ import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemSword;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
 import java.util.ArrayList;
@@ -25,7 +30,8 @@ public class ModItems {
 	
 	//Materials
 	public static final ToolMaterial MATERIAL_RUBY = EnumHelper.addToolMaterial("material_ruby", 15, 2500, 10.0F, 5, 15);
-
+	public static final ArmorMaterial ARMOR_MATERIAL_RUBY = EnumHelper.addArmorMaterial("armor_material_ruby", Reference.MOD_ID + ":ruby", 20,
+	 new int[] {6, 10, 6, 8}, 15, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
 
 
 	//Items
@@ -39,5 +45,9 @@ public class ModItems {
 	public static final ItemAxe RUBY_AXE = new ToolAxe("ruby_axe", MATERIAL_RUBY);
 	public static final ItemHoe RUBY_HOE = new ToolHoe("ruby_hoe", MATERIAL_RUBY);
 
-	
+	//Armour
+	public static final Item RUBY_HELMET = new ArmorBase("ruby_helmet", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.HEAD);
+	public static final Item RUBY_CHESTPLATE = new ArmorBase("ruby_chestplate", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.CHEST);
+	public static final Item RUBY_LEGGINGS = new ArmorBase("ruby_leggings", ARMOR_MATERIAL_RUBY, 2, EntityEquipmentSlot.LEGS);
+	public static final Item RUBY_BOOTS = new ArmorBase("ruby_boots", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.FEET);
 }
