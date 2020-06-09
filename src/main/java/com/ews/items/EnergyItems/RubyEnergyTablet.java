32033ae19e3bf2a1c2ac8e74677d97c6d00f6b48
AlphaWolf2020 <65351771+AlphaWolf2020@users.noreply.github.com>
2020-06-09 23:53:29 +0100

    Trying to fix crash error - not fixed yet

--- src/main/java/com/ews/items/EnergyItems/RubyEnergyTablet.java
+++ src/main/java/com/ews/items/EnergyItems/RubyEnergyTablet.java
@@ -8,27 +8,24 @@ public class RubyEnergyTablet extends EnergyContainerItem{
     public RubyEnergyTablet(String name, int capacity) {
         super(name, capacity);
         
-    //   setUnlocalizedName(name);
-    //      setRegistryName(name);
-    //      setCreativeTab(Main.EWS_TAB);
-    //      
-    //      ModItems.ITEMS.add(this);
-   }
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(Main.EWS_TAB);
+        
+        ModItems.ITEMS.add(this);
+    }
         
         
         
-     //     @Override
-   //       public void registerModels(){
+        @Override
+        public void registerModels(){
             
-   //       Main.proxy.registerItemRenderer(this, 0, "inventory");
-  //        }
-  //    }
-   
-
-
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
+        }
     
-   }
 
-//Mother of fucks?
+}
 
-//  I'm giving u access to it on Curse for this help XDDDD (when i figure out how to...) idk how close.... 
+
+
+// I'm giving u access to it on Curse for this help XDDDD (when i figure out how to...)
